a7bbe86db4f06e7a045416df2deff47b62d0fea8
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-05-22 16:59:54 +0200

    Specs compatible to 1.8.7

--- spec/ustack_spec.rb
+++ spec/ustack_spec.rb
@@ -28,11 +28,11 @@ describe Ustack do
   let :app do
     Ustack.new do
       use M1
-      use M2, option_for_m2: 'option-for-m2'
+      use M2, :option_for_m2 => 'option-for-m2'
     end
   end
 
-  let(:env) { {call_stack: []} }
+  let(:env) { {:call_stack => []} }
 
   it 'correctly calls middleware stack' do
     expect { app.run(env).should eq('from-m1') }.to change { env[:call_stack] }.
@@ -40,7 +40,7 @@ describe Ustack do
   end
 
   it 'swaps middleware' do
-    app.swap(M2, M3, option_for_m3: 'option-for-m3')
+    app.swap(M2, M3, :option_for_m3 => 'option-for-m3')
     expect { app.run(env).should eq('from-m1') }.to change { env[:call_stack] }.
         from([]).to(%w[m1-before option-for-m3 from-m3 m1-after])
   end
@@ -55,14 +55,14 @@ describe Ustack do
     end
 
     it 'inserts before given middleware' do
-      app.insert_before(M2, InsertBefore, option_for_insert_before: 'option-for-insert-before')
+      app.insert_before(M2, InsertBefore, :option_for_insert_before => 'option-for-insert-before')
       expect { app.run(env).should eq('from-m1') }.to change { env[:call_stack] }.
           from([]).to(%w[m1-before option-for-insert-before option-for-m2 from-m2
                          from-insert-before m1-after])
     end
 
     it 'inserts before very first middleware' do
-      app.insert_before(M1, InsertBefore, option_for_insert_before: 'option-for-insert-before')
+      app.insert_before(M1, InsertBefore, :option_for_insert_before => 'option-for-insert-before')
       expect { app.run(env).should eq('from-insert-before') }.to change { env[:call_stack] }.
           from([]).to(%w[option-for-insert-before m1-before option-for-m2 from-m2 m1-after from-m1])
     end
@@ -78,14 +78,14 @@ describe Ustack do
     end
 
     it 'inserts after given middleware' do
-      app.insert_after(M1, InsertAfter, option_for_insert_after: 'option-for-insert-after')
+      app.insert_after(M1, InsertAfter, :option_for_insert_after => 'option-for-insert-after')
       expect { app.run(env).should eq('from-m1') }.to change { env[:call_stack] }.
           from([]).to(%w[m1-before option-for-insert-after option-for-m2 from-m2
                          from-insert-after m1-after])
     end
 
     it 'inserts after very last middleware' do
-      app.insert_after(M2, InsertAfter, option_for_insert_after: 'option-for-insert-after')
+      app.insert_after(M2, InsertAfter, :option_for_insert_after =>'option-for-insert-after')
       expect { app.run(env).should eq('from-m1') }.to change { env[:call_stack] }.
           from([]).to(%w[m1-before option-for-m2 option-for-insert-after
                          from-insert-after from-m2 m1-after])
